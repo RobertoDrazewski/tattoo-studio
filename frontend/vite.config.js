4aff787be20bbd506f6dc99cb0a9998847ef7122
Roberto Drazewski <drazewski.roberto@gmail.com>
2026-06-20 15:39:30 -0300

    Primer commit: subiend5

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,23 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: { '/api': 'http://localhost:4000', '/uploads': 'http://localhost:4000' },
-  },
+export default defineConfig(({ mode }) => {
+  // Carga las variables de entorno
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    server: {
+      port: 5173,
+      // Solo usa el proxy en desarrollo
+      proxy: mode === 'development' ? {
+        '/api': 'http://localhost:4000',
+        '/uploads': 'http://localhost:4000'
+      } : {}
+    },
+    preview: {
+      allowedHosts: ['rickart13.up.railway.app'],
+      port: 5173,
+    }
+  };
 });
